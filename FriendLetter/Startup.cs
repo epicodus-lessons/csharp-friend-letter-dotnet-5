1f41094ecfebb17218754ef9ae5e3dfe980d40c8
Travis Scott <travisty12@gmail.com>
2021-01-24 15:19:09 -0800

    add postcard form

--- FriendLetter/Startup.cs
+++ FriendLetter/Startup.cs
@@ -26,6 +26,7 @@ namespace FriendLetter
 
     public void Configure(IApplicationBuilder app)
     {
+      app.UseDeveloperExceptionPage();
       app.UseRouting();
 
       app.UseEndpoints(routes =>
